ccfcd8c2ed961cb9577c95f5a36bf90e9fd77de0
khushi3 <khushboo.bharti@emids.com>
2017-03-28 15:59:40 +0530

    contact -list component contact-detail comp migrated

---
+++ app/contact-detail-component/contact-detail-component.es5.js
@@ -0,0 +1,12 @@
+angular
+  .module('contact-detail-component', ['ngRoute', 'contacts-service', 'zippy-component'])
+  .config(function ($routeProvider) {
+    $routeProvider.when('/contact/:id', {
+       controller: function (contactsService, $routeParams) {
+        this.contact = contactsService.getContact($routeParams.id);
+      },
+      controllerAs: '$ctrl',
+      template: '<contact-detail-component><contact-detail-component>'
+    });
+  })
+
